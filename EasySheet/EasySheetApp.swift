441eac0e8b0d9aa4e395359c103277f64adbbb5f
Korsun <jenya.korsun@gmail.com>
2022-12-22 09:59:52 +0200

    Initial Commit

---
+++ EasySheet/EasySheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySheetApp.swift
+//  EasySheet
+//
+//  Created by Yevhenii Korsun on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EasySheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
